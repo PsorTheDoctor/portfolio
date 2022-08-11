502a89e3275ccc9432ec00314f578ef8075c35ca
PsorTheDoctor <psor2.0@gmail.com>
2022-08-11 20:15:31 +0200

    make some content for Home component

--- portfolio/src/components/page/Home.tsx
+++ portfolio/src/components/page/Home.tsx
@@ -2,21 +2,51 @@ import styled from "styled-components";
 
 type PropsType = {};
 
-const Wrapper = styled.div`
+const HomeWrapper = styled.div`
   display: flex;
+  flex-direction: column;
   gap: 10px;
 `;
 
-const Column = styled.div``;
+const HomeRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+`;
+
+const ProjectsHeader = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+`;
+
+const ProjectsColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 export const Home = (props: PropsType) => (
-  <Wrapper>
-    <Column>
-      <p>firstcol</p>
-    </Column>
-
-    <Column>
-      <p>secondcol</p>
-    </Column>
-  </Wrapper>
+  <HomeWrapper>
+    <HomeRow>
+      <div>
+        <p>image</p>
+      </div>
+      <div>
+        <p>text</p>
+      </div>
+    </HomeRow>
+    <ProjectsHeader>
+      <h3>PROJECTS</h3>
+    </ProjectsHeader>
+    <HomeRow>
+      <ProjectsColumn>
+        <p>img1</p>
+        <p>text1</p>
+      </ProjectsColumn>
+      <ProjectsColumn>
+        <p>img2</p>
+        <p>text2</p>
+      </ProjectsColumn>
+    </HomeRow>
+  </HomeWrapper>
 );
